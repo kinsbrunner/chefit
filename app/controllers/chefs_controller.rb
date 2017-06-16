bb47bf5e3658e667e46f0fea105bf114450983bd
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2017-06-16 09:59:35 -0400

    Refactoring Chefs controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,6 @@
 class ChefsController < ApplicationController
+  before_action :current_chef, only: [:show, :edit, :update, :destroy]
+
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
@@ -18,16 +20,13 @@ class ChefsController < ApplicationController
   end
   
   def show
-    @chef = Chef.find_by_id(params[:id])
     @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def edit
-    @chef = Chef.find_by_id(params[:id])
   end
   
   def update
-    @chef = Chef.find_by_id(params[:id])
     if @chef.update(chef_params)
       flash[:success] = "Your account was updated successfully!"
       redirect_to @chef
@@ -37,13 +36,16 @@ class ChefsController < ApplicationController
   end  
   
   def destroy
-    @chef = Chef.find_by_id(params[:id])
     @chef.destroy
     flash[:danger] = "The chef has been deleted with all its associated recipes!"
     redirect_to chefs_path
   end
     
   private
+
+  def current_chef
+    @chef = Chef.find_by_id(params[:id])
+  end
   
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
